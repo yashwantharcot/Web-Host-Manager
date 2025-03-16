236d24ce6a2021c0db917acdebb93097e2599d98
yashwantharcot <yeshuarcot2003@gmail.com>
2025-03-16 21:56:07 +0530

    Switched from MongoDB to PostgreSQL and updated configuration

--- WebHostManager/backend/models/User.js
+++ WebHostManager/backend/models/User.js
@@ -1,18 +1,1 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'staff', 'client'], default: 'client' }
-});
-
-// Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-module.exports = mongoose.model('User', UserSchema);
